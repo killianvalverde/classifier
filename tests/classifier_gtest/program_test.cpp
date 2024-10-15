5b621b51eeed1248d1a550da043e7abb2e0c178d
Killian Valverde <KillianValverde@gmail.com>
2024-10-15 09:11:39 +0200

    src: add implementation template

---
+++ tests/classifier_gtest/program_test.cpp
@@ -0,0 +1,53 @@
+/* classifier
+ * Copyright (C) 2024 Killian Valverde.
+ *
+ * This file is part of classifier.
+ *
+ * classifier is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * classifier is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with classifier. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file        classifier_gtest/program_test.cpp
+ * @brief       program unit test.
+ * @author      Killian Valverde
+ * @date        2024/10/15
+ */
+
+#include <gtest/gtest.h>
+
+#include "classifier/program.hpp"
+
+
+class classifier_program : public ::testing::Test
+{
+protected:
+    classifier_program()
+            : prog()
+    {
+    }
+
+    void SetUp() override
+    {
+    }
+
+    classifier::program prog;
+};
+
+
+TEST_F(classifier_program, execute)
+{
+    // int ret = -1; 
+    // EXPECT_NO_THROW(ret = prog.execute());
+    // EXPECT_TRUE(ret == 0);
+}
